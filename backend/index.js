92b837d9ff2fd11f0e072d424a353c5ac6f8e928
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2024-07-13 02:37:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
-const corsOption={
+
+app.use(cors({
     origin:'https://clumsy-chats.vercel.app',
     credentials:true
-};
-app.use(cors(corsOption)); 
+})); 
 
 
 // routes
