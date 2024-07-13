c3fb1be827ebdc60b79f6da3c559f494cd21d5f5
BOSON-18 <deeshankbatra663@gmail.com>
2024-07-13 15:20:40 +0530

    Persist

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,13 +63,14 @@ export const login = async (req, res) => {
             userId: user._id
         };
 
-        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        const token =  jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: false, sameSite: 'strict' }).json({
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
-            profilePhoto: user.profilePhoto
+            profilePhoto: user.profilePhoto,
+           
         });
 
     } catch (error) {
