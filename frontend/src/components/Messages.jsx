20b0062cf5a28c9ef916dd48d491830d769d8818
BOSON-18 <deeshankbatra663@gmail.com>
2024-07-13 15:38:15 +0530

    optional cahingi

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -17,12 +17,13 @@ const Messages = () => {
 
     // }
     messages.filter(message => message._id !== undefined && message._id !== null);
+    // if(messages.length<1)
     const safeMessages = Array.isArray(messages) ? messages : [];
 
     return (
         <div className='px-4 flex-1 overflow-auto'>
             {
-                safeMessages.map((message) => {
+                safeMessages?.map((message) => {
                     return (
                         <Message key={message._id} message={message} />
                     )
